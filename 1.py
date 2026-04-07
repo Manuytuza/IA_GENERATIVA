03997f15b18ac5fc46fee81b50e62ae544329d83
Manuel Ytuza <alejo.ytuza@gmail.com>
2026-04-06 22:21:33 -0500

    primerdia

---
+++ 1.py
@@ -0,0 +1,10 @@
+#primera clase 06/04 IA
+#Nootbock LLM proyecto final
+#Tareas diarias y trabajos diarios 
+#grupos de trabajo y estudio
+#verificar entregas enviadas
+#se suben los trabajos en grupo de forma individual 
+#10 de junio presentacion de proyecto final 
+##Dato: Es un elemento en bruto, sin interpretar. Datos + contexto = información 
+#Zapier Y N8N IR ANALIZANDO
+##CONFIGURAR gpu con CUDA
