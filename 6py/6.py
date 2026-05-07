ae656bcf5837b321fb382b4ca6e2840c90acbe63
Manuel Ytuza <alejo.ytuza@gmail.com>
2026-05-06 22:07:07 -0500

    fin clas 06/05

---
+++ 6py/6.py
@@ -0,0 +1,187 @@
+#Analitica 
+#OPEN AI SE PAGARA
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#gráficos básicos incorporados en PANDAS
+df1 = pd.read_csv('df1', index_col=0)
+
+df2 = pd.read_csv('df2')
+#print(df2.info())
+
+df3 = pd.read_csv('df3')
+#print(df2.info())
+
+#crear histograma a partir del df - si hay seaborn lo toma como base
+#df1['A'].hist()
+#plt.show()
+#df1['A'].plot.hist()
+#plt.show()
+#manera alterna B
+#df1['A'].plot(kind='hist',              bins=50 )
+#plt.show()
+#euristica no es exacto pero es desente 
+##Tipos de plots sia born
+tips = sns.load_dataset('tips')
+print(tips.shape)
+tips.head()
+"""
+# gráfico de líneas
+tips['total_bill'].plot(kind="hist")
+plt.title('Total de la Cuenta a lo Largo del Tiempo')
+plt.show()
+
+# área
+df2.plot.area(alpha=0.6)
+plt.show()
+# área: manera alterna MEJOR FORMA
+df2.plot(kind='area',
+         alpha=0.6)
+plt.show()
+"""
+# barras: en paralelo, con recorte de filas
+df3_p10 = df3.iloc[:10] #iloc si excluye al ultimo 10 
+#df3_p10.plot(kind='bar') # alternativa: df3_p10.plot.bar()
+
+# barra apilada - haciendo recorte de filas
+df3_p10.plot(kind='bar',
+             stacked=True #apila
+             )
+    # alternativa: df3_p10.plot.bar(stacked=True)
+plt.show()
+
+# series de tiempo: alternativa
+df1.plot(kind='line',
+         y='C')
+plt.show()
+
+# scatter (nube de puntos)
+df1.plot(kind='scatter',
+         x='A', y='C')
+# alternativa: df1.plot.scatter(x='A', y='C')
+
+# boxplot (cajas y vigotes)
+df3.plot(kind='box')
+
+#####
+#1. Crear un Dataframe de Pandas que contenga
+#Columna NATURAL - Numeros naturales del 1 al 20
+#Columna PRIMOS - Los primeros 20 números primos
+
+import pandas as pd
+
+a = [x for x in range(1,21)]
+#print(a)
+b = primos = [ #by chat-gpt
+    x for x in range(2, 100)
+    if all(x % y != 0 for y in range(2, x)) #devuelve True si todo es True
+][:20]
+
+df = pd.DataFrame({
+    "naturales": a,
+    "primos" : b
+})
+print(df)
+
+#######MATPLOTLIB
+# Crear datos para el gráfico
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Crear el gráfico
+plt.plot(x, y)
+
+plt.title('Gráfico de Seno')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+
+plt.show()
+
+#Barra Vertical
+# Crear datos para el gráfico
+frutas = ['Manzanas', 'Plátanos', 'Naranjas', 'Peras']
+cantidad = [20, 25, 15, 30]
+# Crear el gráfico de barras verticales
+plt.bar(frutas, cantidad)
+plt.title('Cantidad de Frutas')
+plt.xlabel('Frutas')
+plt.ylabel('Cantidad')
+plt.show()
+
+#Barra Horizontal
+# Crear el gráfico de barras horizontales
+plt.barh(frutas, cantidad)
+plt.title('Cantidad de Frutas')
+plt.xlabel('Cantidad')
+plt.ylabel('Frutas')
+plt.show()
+
+#Pie
+# Crear el gráfico de torta
+plt.pie(cantidad, labels=frutas, autopct='%1.1f%%') #porcentaje dentro de pie
+plt.title('Proporción de Frutas')
+plt.show()
+
+#Histograma
+# Crear datos para el histograma
+datos = np.random.normal(0, 1, 1000) #siempre es causiana normal 
+
+# Crear el histograma
+plt.hist(datos, bins=20)
+
+plt.title('Histograma de Datos')
+plt.xlabel('Valor')
+plt.ylabel('Frecuencia')
+
+plt.show()
+#Nube de puntos (scatter plot)
+
+# Crear datos para el gráfico de dispersión
+x = np.random.rand(50)
+y = np.random.rand(50)
+
+# Crear el gráfico de dispersión
+plt.scatter(x, y)
+
+plt.title('Gráfico de Dispersión')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+#####SEABORN
+
+# algunos datasets están pre-cargados en Seaborn (ej 'tips')
+tips = sns.load_dataset('tips')
+tips.head()
+
+#scatter
+sns.scatterplot(data=tips,
+                x='total_bill', y='tip')
+
+plt.title('Propina vs Total de la Cuenta')
+
+plt.show()
+#Histplot
+# Crear un gráfico displot
+sns.histplot(data=tips, x='total_bill',
+             kde=True, #grafico de curva
+             bins='auto'
+             )
+plt.title('Distribución del Total de la Cuenta')
+plt.show()
+
+##Displot
+"""
+para distribución de datos numéricos
+principales: histograma, kde, ecdf
+alternativa (pronto será inválida): sns.distplot()"""
+# Crear un gráfico displot
+sns.displot(data=tips,
+            x='total_bill',
+            kind='hist', # hist (defeault), ecdf, kde
+            kde=True
+            )
+plt.title('Distribución del Total de la Cuenta')
+plt.show()
